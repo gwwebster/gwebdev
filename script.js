9720611683612f0c623594cf05dcfccd84197905
George <gwwebster23@gmail.com>
2023-02-19 14:54:58 -0600

    Resume html + css + js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Sticky banner
+
+window.onscroll = function () {stickyHeader()};
+const header = document.getElementById("banner");
+
+// Get offset position of banner
+const sticky = header.offsetTop; 
+
+// Add sticky class to header when scroll position reached, remove sticky class when you leave scroll position
+function stickyHeader() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
 
